178cc0d10463554ec77ce62e6d7b2de649271176
nemesis <nemesis.mk47@gmail.com>
2025-03-29 17:33:22 +0300

    Use EMPracticeTheme in MainActivity

--- app/src/main/java/com/nemesis/empractice/MainActivity.kt
+++ app/src/main/java/com/nemesis/empractice/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Surface
+import com.nemesis.empractice.ui.theme.EMPracticeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            EMPracticeTheme {
+                Surface {
+                    
+                }
+            }
         }
     }
 }

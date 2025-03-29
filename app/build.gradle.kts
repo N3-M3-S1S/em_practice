215e5bae7887eabbd158f63e99c5ef644207a262
nemesis <nemesis.mk47@gmail.com>
2025-03-29 16:14:40 +0300

    Add RXJava dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.rxandroid)
+    implementation(libs.rxjava)
     testImplementation(libs.junit)
     testImplementation(libs.coroutines.test)
     androidTestImplementation(libs.androidx.junit)

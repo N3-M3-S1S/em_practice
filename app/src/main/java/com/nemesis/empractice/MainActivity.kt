47d36f14fe43d81f557c64c92ee8e3d544796c89
nemesis <nemesis.mk47@gmail.com>
2025-03-29 19:31:35 +0300

    Prepare for working with views instead of compose

--- app/src/main/java/com/nemesis/empractice/MainActivity.kt
+++ app/src/main/java/com/nemesis/empractice/MainActivity.kt
@@ -2,21 +2,11 @@ package com.nemesis.empractice
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Surface
-import com.nemesis.empractice.ui.theme.EMPracticeTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(R.layout.main) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            EMPracticeTheme {
-                Surface {
-                    
-                }
-            }
-        }
     }
+
 }
